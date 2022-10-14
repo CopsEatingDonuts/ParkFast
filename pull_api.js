09d105c139b21e09414f31f63da8dbda9ebe9d25
yankai <yankailim00@gmail.com>
2022-10-14 14:08:09 +0800

    extract data, store in dictionary format

--- pull_api.js
+++ pull_api.js
@@ -2,11 +2,17 @@ var http = require('http');
 var util = require('util');
 var exec = require('child_process').exec;
 
-var command = 'curl "https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability" -H "AccessKey: dd528ffa-bad4-4e39-baeb-dfb9804afea2" -H "Token: XRH7A54-+Juawg4dQ58bY5asyfmfe@4PeZdzdn4aF0td8U42fX9d22RacCawKtW4bddf3584a44hduN@abdbe43@AvNf5fyz@7FV"'
+var x = "data-123-abc-def";
+console.log(x.split('-'));
+
+var command = 'curl "https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability" -H "AccessKey: dd528ffa-bad4-4e39-baeb-dfb9804afea2" -H "Token: pXjybfxqJ@bwnCbmtafm3227e9ae@juF2@fRqf0taVQb@N4eb2b-2T3bBc34K8efQv23642-9urvevr-6dkgjbbN9hD49-382dqn"'
 child = exec(command, function(error, stdout, stderr){
 
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+    dictionary = stdout.replace('{"Status":"Success","Message":"","Result":[{', "");
+    dictionary = dictionary.replace('}]}','');
+    console.log(dictionary);
+    dictionary = dictionary.split('},{');
+    console.log(dictionary);
     
     if(error !== null)
     {
