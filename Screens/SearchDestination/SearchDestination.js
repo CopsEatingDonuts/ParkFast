fa1f161191270828b6f2930e1d7333a3fae8375f
serphyshio <yifei.colin@gmail.com>
2022-10-19 12:06:44 +0800

    added api for search destinations

--- Screens/SearchDestination/SearchDestination.js
+++ Screens/SearchDestination/SearchDestination.js
@@ -1,1 +1,26 @@
-//javascript
+function passDestination(){
+    var dest = document.getElementById("dest").value;
+    localStorage.setItem("textvalue", dest);
+    return;
+}
+var searchInput = "dest";
+
+$(document).ready(function () {
+ var autocomplete;
+ autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+  componentRestrictions: {'country': ['SG']},
+ });
+  
+ google.maps.event.addListener(autocomplete, 'place_changed', function () {
+  var near_place = autocomplete.getPlace();
+ });
+});
+
+/*let autocomplete;
+function initAutocomplete(){
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById('dest'),{
+        types: ['geocode'],
+        componentRestrictions: {'country': ['SG']},
+        fields: ['place_id', 'geometry', 'name']     
+    });
+}*/
