6a5e6333eaa1def2985f8ac1c0ace11d28432f97
Player999058 <ansonawk@gmail.com>
2022-10-20 16:01:49 +0800

    added links and password checks

---
+++ Screens/Login/Styles/login.js
@@ -0,0 +1,22 @@
+function validateLogin(){
+    var pw = document.getElementById("pswd").value; 
+    var user = document.getElementById("username").value;
+
+    if(user == "") {  
+        document.getElementById("usermessage").innerHTML = "*Enter Username";  
+        document.getElementById("message").innerHTML = "";  
+        return false;  
+    }  
+      
+    //check empty password field  
+    if(pw == "") {  
+      document.getElementById("message").innerHTML = "*Enter Password";  
+      document.getElementById("usermessage").innerHTML = "";  
+      return false;  
+    }  
+
+    else{
+        alert("Log in successful!");
+        window.open("http://127.0.0.1:5502/Screens/SearchDestination/SearchDestination.html", "_self");
+    }
+}
