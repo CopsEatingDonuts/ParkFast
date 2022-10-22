225e5a81e4dec74359194b30c8e4af652b16c2b9
serphyshio <yifei.colin@gmail.com>
2022-10-23 00:21:53 +0800

    added firebase to login and sign up

---
+++ Screens/login and Signup with firebase/login.js
@@ -0,0 +1,17 @@
+function validateLogin(email, pw){
+
+    if(email == "") {  
+        document.getElementById("usermessage").innerHTML = "*Enter Username";  
+        document.getElementById("message").innerHTML = "";  
+        return false;  
+    }  
+      
+    //check empty password field  
+    if(pw == "") {  
+      document.getElementById("message").innerHTML = "*Enter Password";  
+      document.getElementById("usermessage").innerHTML = "";  
+      return false;  
+    }  
+    return true;
+}
+    
