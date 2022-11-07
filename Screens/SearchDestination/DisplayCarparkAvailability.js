6c7c7b20ba8438814716e6ee66e8245006981a8f
Player999058 <ansonawk@gmail.com>
2022-11-07 16:07:19 +0800

    added ui for feedback

--- Screens/SearchDestination/DisplayCarparkAvailability.js
+++ Screens/SearchDestination/DisplayCarparkAvailability.js
@@ -1,6 +1,22 @@
 document.getElementById("result_carpark").innerHTML = sessionStorage.getItem("carparkValue");
 var carPark_location = sessionStorage.getItem("carparkValue");
 
+const openModal = document.querySelector('.upload-image');
+const closeModal = document.querySelector('.close');
+const closeModal1 = document.querySelector('.submit');
+const modal = document.querySelector('#modal');
+
+openModal.addEventListener('click', () => {
+  modal.showModal();
+})
+
+closeModal.addEventListener('click', () => {
+  modal.close();
+})
+
+closeModal1.addEventListener('click', () => {
+  modal.close();
+})
 var map;
 function initMap() {
     var latlng = new google.maps.LatLng(1.3521, 103.8198);
@@ -15,8 +31,13 @@ function initMap() {
     map,
     });
     
-    
 }
 
+var imgBox1 = document.getElementById("imgbox1");
+var upload1 = document.getElementById("upload1");
+var loadFile = function(event){
+  imgBox1.style.backgroundImage = "url(" +URL.createObjectURL(event.target.files[0])+ ")";
+  upload1.style.maxWidth = auto;
+}
 
     
