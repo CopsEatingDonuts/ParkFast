81b86d0f1c978d9dbae07ea252ebd9836616d1be
yankai <yankailim00@gmail.com>
2022-11-09 17:00:11 +0800

    remove quotation marks from parkingrate

--- Application Skeleton/control/DisplayCarparkAvailability.js
+++ Application Skeleton/control/DisplayCarparkAvailability.js
@@ -201,11 +201,7 @@ catch (err) {}
 
 var map;
 function initMap() {
-    console.log(selected_carpark_lat_wgs84);
-    console.log(selected_carpark_lng_wgs84);
     var latlng = new google.maps.LatLng(selected_carpark_lat_wgs84, selected_carpark_lng_wgs84);
-    console.log(latlng1);
-    console.log(latlng);
     var mapOptions = {
       zoom: 15,
       center: latlng
@@ -289,4 +285,4 @@ var parkingRates = selected_carpark.parkingRate[0];
 var satdayRate = "satdayRate : " + parkingRates.satdayRate + " || ";
 var sunPHRate = "sunPHRate : " + parkingRates.sunPHRate + " || ";
 var weekdayRate = "weekdayRate : " + parkingRates.weekdayRate + " || ";
-carparkRate.innerHTML = JSON.stringify(weekdayRate + satdayRate + sunPHRate);
+carparkRate.innerHTML = weekdayRate + satdayRate + sunPHRate;
